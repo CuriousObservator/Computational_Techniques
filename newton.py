44fbe558409708c13080bd7bc3095fc071e3e4c3
CuriousObservator <mvrabhishek@gmail.com>
2025-12-19 04:52:55 +0530

    updated Newton-Raphson method

--- newton.py
+++ newton.py
@@ -5,22 +5,31 @@ Created on Sun Aug 30 12:59:41 2020
 @author: Abhishek
 """
 import math as m
-def f(i):
-    p=((i**m.log(i,m.e))-(m.e**m.sin(i))-(50*i))    
+
+def f(x):
+    p=(pow(m.e, -0.2*x)*m.sin(5*x+2)-(x/10))
     return(p)
     
-def fd(i):
-    pd=((2*(i**(m.log(i,m.e)-1))*(m.log(i,m.e)))-(m.e**(m.sin(i))*m.cos(i))-50)
-    return(pd)
+def df(x):
+    term1 = -0.2 * m.exp(-0.2 * x) * m.sin(5 * x + 2)
+    term2 = 5 * m.exp(-0.2 * x) * m.cos(5 * x + 2)
+    return term1 + term2 - 0.1
+
+
 xcur=float(input("Please provide the guess"))
 thr=float(input("Please provide the threshold error"))
 ea=100
+
 while(ea>thr):
-    newx=xcur-(f(xcur)/fd(xcur))
+    newx=xcur-(f(xcur)/df(xcur))
     ea=abs(((newx-xcur)/newx)*100)
-    print(ea)
-    print(newx)
-    print(f(newx))
-    print(xcur)
-    print(f(xcur))
+    # print(ea)
+    # print(newx)
+    # print(f(newx))
+    # print(xcur)
+    # print(f(xcur))
     xcur=newx
+    print(f"Root estimate: {newx:.6f} | Error: {ea:.10f}")
+
+print(f"Final Root estimate: {newx:.6f} | Error: {ea:.10f}")
+    
