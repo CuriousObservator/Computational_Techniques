51559eae74f13100eb3d3d0b110c94c3670f7fa3
CuriousObservator <mvrabhishek@gmail.com>
2025-12-19 05:12:41 +0530

    updated Secant method

--- secant.py
+++ secant.py
@@ -5,9 +5,10 @@ Created on Sun Aug 30 13:51:06 2020
 @author: Abhishek
 """
 import math as m
-def f(i):
-    p=((i**m.log(i,m.e))-(m.e**m.sin(i))-(50*i))    
+def f(x):
+    p=(pow(m.e, -0.2*x)*m.sin(5*x+2)-(x/10))
     return(p)
+
 
 xcur=float(input("Please provide guess 1"))
 xprev=float(input("Provide guess 2"))
@@ -15,17 +16,18 @@ thr=float(input("Please provide the threshold error"))
 ea=100
 
 while(ea>thr):
-    xfut=xcur-(f(xcur)/abs((f(xcur)-f(xprev))/(xcur-xprev)))
+    xfut=xcur-((f(xcur)*(xprev - xcur))/(f(xprev)-f(xcur)))
     ea=abs(((xfut-xcur)*100)/xcur)
-    print(xcur)
-    print(xfut)
-    print(xprev)
-    print(f(xcur))
-    print(ea)
+    # print(xcur)
+    # print(xfut)
+    # print(xprev)
+    # print(f(xcur))
+    # print(ea)
     xprev=xcur
     xcur=xfut
+    print(f"Root estimate: {xcur:.6f} | Error: {ea:.10f}%")
        
-    
+print(f"Final Root estimate: {xcur:.6f} | Error: {ea:.10f}%")
 
     
     
