6670e68410b136b4dc7bf8809189a56b9ba6c469
CuriousObservator <mvrabhishek@gmail.com>
2025-12-23 22:31:33 +0530

    Updated Lagrange Interpolation

--- lagrange.py
+++ lagrange.py
@@ -10,8 +10,8 @@ datavalues=[0.1411,-0.6878,-0.9962,-0.5507,-0.3115]
 datapoints=[1,1.3,1.6,1.9,2.2]
 
 #for m in range(0,int(n)):
-#    datapoints.append(float(input("Put in the independent variables. ")))
-#    datavalues.append(float(input("Put in the data values. ")))
+#    datavalues.append(float(input("Put in the independent variables. ")))
+#    datapoints.append(float(input("Put in the data values. ")))
 
 def Lag(i,x,k):
     j=0
@@ -29,13 +29,3 @@ sum=0
 for p in range(0,(int(l)+1)):
     sum=sum+(Lag(p, x, l+1)*datavalues[p])
 print(sum)
-#i=0
-#j=0
-#diff=[]
-#for i in datapoints:
-#    diff.append(abs(x-i))
-
-#diff.ascend()
-
-#for p in range(0,(int(l)+1))
-#    sum=sum+Lag(p, x, l+1)*data   
